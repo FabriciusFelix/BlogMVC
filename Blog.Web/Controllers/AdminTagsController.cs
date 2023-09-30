7d5d923d4a6bbfe67c2567429d63fd0ed2ceb9ec
Fabricius Felix <Fabricius.felix@gmail.com>
2023-09-30 16:15:39 -0300

    Adicionar arquivos de projeto.

---
+++ Blog.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,38 @@
+﻿
+using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly BlogDbContext _dbContext;
+        public AdminTagsController(BlogDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            //var name = Request.Form["name"];
+
+            //var displayName = Request.Form["displayName"];  html: <input type="text" class="form-control" id="displayName" name="displayName" />
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            _dbContext.Tags.Add(tag);
+            return View("Add");
+        }
+    }
+}
